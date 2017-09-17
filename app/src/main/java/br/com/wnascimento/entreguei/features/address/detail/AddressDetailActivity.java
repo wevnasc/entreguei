f93e14287c3b4131e219c606ecbc74d8e58464fe
Weverson Nascimento <weverson.sn@gmail.com>
2017-09-17 17:46:54 -0300

    back button detail activity

--- app/src/main/java/br/com/wnascimento/entreguei/features/address/detail/AddressDetailActivity.java
+++ app/src/main/java/br/com/wnascimento/entreguei/features/address/detail/AddressDetailActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -33,6 +35,9 @@ public class AddressDetailActivity extends AppCompatActivity {
     @BindView(R.id.city_and_state_text)
     TextView cityAndStateText;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     public static void start(Activity context, Address address, Pair<View, String>... views) {
         Intent starter = new Intent(context, AddressDetailActivity.class);
         starter.putExtra(EXTRA_ADDRESS, address);
@@ -45,7 +50,17 @@ public class AddressDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_detail);
         ButterKnife.bind(this);
+        initToolbar();
         showCollaboratorDetail();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setTitle(R.string.title_address_detal);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     private void showCollaboratorDetail() {
