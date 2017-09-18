8be23f0a196ccbc7c15e0817afe212e9312cb8b0
Weverson Nascimento <weverson.sn@gmail.com>
2017-09-18 12:04:15 -0300

    Update ValidateUtil.java

--- app/src/main/java/br/com/wnascimento/entreguei/util/ValidateUtil.java
+++ app/src/main/java/br/com/wnascimento/entreguei/util/ValidateUtil.java
@@ -6,7 +6,7 @@ import android.util.Patterns;
 public class ValidateUtil {
 
     public static boolean isValidateEmail(String email) {
-        return email.contains("gmail.com") && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        return email.contains("br.ibm.com") && Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
     public static boolean validateMinLength(String string, int maxLength) {
